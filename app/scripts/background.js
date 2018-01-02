3bcf6afc0faa6278cf3113e35aea58fca214a7ab
greatghoul <greatghoul@gmail.com>
2018-01-03 01:56:50 +0800

    Show simple counter list.

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -5,8 +5,4 @@ chrome.runtime.onInstalled.addListener((details) => {
   console.log('previousVersion', details.previousVersion)
 })
 
-chrome.browserAction.setBadgeText({
-  text: `'Allo`
-})
-
 console.log(`'Allo 'Allo! Event Page for Browser Action`)
--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,4 +1,24 @@
 // Enable chromereload by uncommenting this line:
 // import 'chromereload/devonly'
 
-console.log(`'Allo 'Allo! Popup`)
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const counters = [
+  { name: 'test 1', count: 33 },
+  { name: 'test 2', count: 112 },
+];
+
+function Counter (props) {
+  return <li>
+    <span class="actions">{props.count}</span>
+    <span class="name">{props.name}</span>
+  </li>;
+}
+
+var counterItems = counters.map(counter => Counter(counter));
+
+ReactDOM.render(
+  <ul class="counters">{counterItems}</ul>,
+  document.getElementById('app')
+);
